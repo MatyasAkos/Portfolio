85b3f0802b5e8af42b72274eb055a88c878ee170
MatyasAkos <matyas.akos.23f@szlgbp.hu>
2024-06-12 16:18:34 +0200

    asd

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const vid = document.querySelector('video')
+const prompt = document.querySelector('p#prompt')
+const vid = document.querySelector('video#kareszvideo')
 const mozaik = document.querySelector('button#mozaik')
+const fa = document.querySelector('button#fa')
+const haz = document.querySelector('button#haz')
+const labirintus = document.querySelector('button#labirintus')
 
-mozaik.addEventListener('click', OnClick);
+const prompts = {
+    'mozaik' : 'Feladat: Karesz rajzolja meg <a rel="_blank" href="https://molnatt.github.io/logo_mozaik_korongok.png">ezt</a> az ábrát'
+    /*'fa' : 
+    'haz' : 
+    'labirintus' : */
+}
 
-function OnClick(){
-    console.log('test')
+mozaik.addEventListener('click', () => OnClick('mozaik'));
+fa.addEventListener('click', () => OnClick('fa'));
+haz.addEventListener('click', () => OnClick('haz'));
+labirintus.addEventListener('click', () => OnClick('labirintus'));
+
+function OnClick(filename){
+    prompt.innerHTML = prompts[filename]
+    vid.src = `./../resources/videos/${filename}.mp4`;
 }
