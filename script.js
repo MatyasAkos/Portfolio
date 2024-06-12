6894e19d3502be15bbceee3e065d7ce47c12416a
MatyasAkos <matyas.akos.23f@szlgbp.hu>
2024-06-12 11:07:57 +0200

    Started working on js

---
+++ script.js
@@ -0,0 +1,8 @@
+const vid = document.querySelector('video')
+const mozaik = document.querySelector('button#mozaik')
+
+mozaik.addEventListener('click', OnClick);
+
+function OnClick(){
+    console.log('test')
+}
